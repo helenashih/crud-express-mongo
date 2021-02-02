1048271ba064b68e9fbcc295e9fae9103740b2f9
helenashih <42580758+helenashih@users.noreply.github.com>
2021-02-01 19:29:27 -0500

    Initial commit

---
+++ public/main.js
@@ -0,0 +1,43 @@
+/* eslint-env browser */
+// main.js
+const update = document.querySelector('#update-button')
+const deleteButton = document.querySelector('#delete-button')
+const messageDiv = document.querySelector('#message')
+
+update.addEventListener('click', _ => {
+  fetch('/quotes', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: 'Andy Dufresne',
+      quote: 'That’s The Beauty Of Music. They Can’t Get That From You.'
+    })
+  })
+    .then(res => {
+      if (res.ok) return res.json()
+    })
+    .then(response => {
+      window.location.reload(true)
+    })
+})
+
+deleteButton.addEventListener('click', _ => {
+  fetch('/quotes', {
+    method: 'delete',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: 'Andy Dufresne'
+    })
+  })
+    .then(res => {
+      if (res.ok) return res.json()
+    })
+    .then(response => {
+      if (response === 'No quote to delete') {
+        messageDiv.textContent = 'No Andy Dufresne quote to delete'
+      } else {
+        window.location.reload(true)
+      }
+    })
+    .catch(console.error)
+})
